c6a4031da5dd92cdfbe28721ab3b466384e3634f
wparedesgt <wparedesgt@gmail.com>
2018-06-21 12:20:25 -0600

    Segundo Grafico

--- analisis.R
+++ analisis.R
@@ -1,7 +1,13 @@
 library(tidyverse)
 load("rda/jodidata.rda")
 
+By_Time_Period <- jodidata %>% group_by(TIME_PERIOD)
 
-jodidata %>%  ggplot() + geom_line(aes(TIME_PERIOD, OBS_VALUE, group = UNIT_MEASURE),  color = "grey50" )
+SUM_OBS <- summarise(By_Time_Period, OBS_VALUE = sum(OBS_VALUE))
 
+AVG_OBS <- summarise(By_Time_Period, OBS_VALUE = mean(OBS_VALUE))
+
+jodidata %>%  ggplot() + geom_line(aes(TIME_PERIOD,OBS_VALUE,group = 1 ),show.legend = FALSE, alpha = 0.2, size = 1) + geom_line(mapping = aes(TIME_PERIOD, OBS_VALUE, group = 1),  data = AVG_OBS, size = 1, color = "black")
+
+AVG_OBS %>% ggplot() + geom_line(mapping = aes(TIME_PERIOD, OBS_VALUE, group = 1),  data = AVG_OBS, size = 1, color = "black")
 
--- wrangle-data.R
+++ wrangle-data.R
@@ -2,5 +2,5 @@ library(tidyverse)
 load("rda/jodidata.rda")
 
 jodidata <- jodidata[order(jodidata$TIME_PERIOD), ]
-save(jodidata, file = "rda/jodidata.rda")
 
+save(jodidata, file = "rda/jodidata.rda")
